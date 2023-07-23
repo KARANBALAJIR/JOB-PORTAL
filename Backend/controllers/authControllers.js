5acd2323145fd6c3b31df442d8d6b782c2166ca5
KARANBALAJIR <karanbalaji.r2021cseb@sece.ac.in>
2023-07-24 00:28:07 +0530

    changes made in controlller, model

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -4,8 +4,8 @@ const ErrorResponse = require("../utils/errorResponse");
 
 
  exports.signup = async (req, res, next) => {
-    const {email} = req.body;
-    const userExist = await User.findOne({email});
+    const { email } = req.body;
+    const userExist = await User.findOne({ email });
     if (userExist) {
         return next(new ErrorResponse("E-mail already registered", 400));
     }
@@ -23,25 +23,25 @@ const ErrorResponse = require("../utils/errorResponse");
 exports.signin = async (req, res, next) => {
     
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
         //validation
         if (!email) {
-             return next(new ErrorResponse("Please add a email", 403));
+             return next(new ErrorResponse("Please add an email", 403));
         }
         if (!password) {
             return next(new ErrorResponse("Please add a password", 403));
        }
 
        //check user email
-       const user = await User.findOne({email});
+       const user = await User.findOne({ email });
        if (!user) {
-        return next(new ErrorResponse("invalid credentials", 400));
+        return next(new ErrorResponse("Invalid credentials", 400));
      }
 
       //check password
       const isMatched = await user.comparePassword(password);
       if (!isMatched) {
-        return next(new ErrorResponse("invalid credentials", 400));
+        return next(new ErrorResponse("Invalid credentials", 400));
      }
 
      sendTokenResponse(user, 200, res);
@@ -52,10 +52,10 @@ exports.signin = async (req, res, next) => {
     }
 }
 
-const sendTokenResponse = async (user, codeStatus, res)=>{
+const sendTokenResponse = async (user, codeStatus, res) => {
     const token = await user.getJwtToken();
     res
-    .status(codeStatus)
-    .cookie("token", token, {maxAge: 60 * 60 * 1000, httponly: true})
-    .json({success : true, token, user})
+        .status(codeStatus)
+        .cookie('token', token, {maxAge: 60 * 60 * 1000, httpOnly: true})
+        .json({success : true, token, user})
 }
