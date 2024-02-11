550bc0479837f569a31810d891f510262d5c5dfd
KARANBALAJIR <karanbalaji.r2021cseb@sece.ac.in>
2024-02-12 00:05:43 +0530

    chnages made

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,6 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => console.log("DB Connected"))
         .catch((err) => console.log(err));
 
-
 //port
 const port = process.env.PORT || 9001;
 
