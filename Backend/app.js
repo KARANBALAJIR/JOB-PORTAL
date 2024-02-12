f560a1893cd7505645d6fa9ff33a60d70fb5af56
KARANBALAJIR <karanbalaji.r2021cseb@sece.ac.in>
2024-02-13 00:10:24 +0530

    Changes made

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
 require("dotenv").config();
 var cors = require("cors");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
+
+
+
+//import routes
+const authRoutes = require("./Routes/authRoutes");    
 
 //database connection
 mongoose.connect(process.env.DATABASE, {
@@ -17,10 +23,31 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => console.log("DB Connected"))
         .catch((err) => console.log(err));
 
+
+//MIDDLEWARE
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ 
+        limit: "5mb",
+        extended: true 
+}));
+app.use(cookieParser());
+app.use(cors());
+
+
+// Routes Middleware
+// app.get("/", (req, res) => {
+//         res.send("Hello from node");
+// })
+app.use("/api", authRoutes);
+
+//error middleware
+app.use(errorHandler);
+
 //port
 const port = process.env.PORT || 9001;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+        console.log(`Server is running on port ${port}`);
 });
 
